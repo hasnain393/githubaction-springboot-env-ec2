ae3e397369f96ad8b8d06ec8e87d91a6ddeb02ab
Hasnain Shaikh <56108097+hasnain393@users.noreply.github.com>
2023-10-23 08:12:22 -0400

    Add files via upload

---
+++ src/main/java/com/demo/controller/ExampleController.java
@@ -0,0 +1,30 @@
+package com.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class ExampleController {
+
+   
+    
+    @GetMapping(value = "/")
+    public String hello() {
+    	return "Hello World";
+    }
+    
+    
+    @GetMapping(value = "/test")
+    public String process() {
+    	return "processing request-aws !!";
+    }
+
+    
+   
+    
+  
+}
