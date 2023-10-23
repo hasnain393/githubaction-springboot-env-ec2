8a9a51c632433d81b13782b9d7a8a5452414aa9c
Hasnain Shaikh <56108097+hasnain393@users.noreply.github.com>
2023-10-23 15:27:49 -0400

    Update ExampleController.java

--- src/main/java/com/demo/controller/ExampleController.java
+++ src/main/java/com/demo/controller/ExampleController.java
@@ -14,7 +14,7 @@ public class ExampleController {
     
     @GetMapping(value = "/")
     public String hello() {
-    	return "Hello World modified v2  by jaya kumar";
+    	return "Hello World modified v2  ";
     }
     
     
